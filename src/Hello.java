a9482ab2fef347cab30aa2707327b1a6a422be9a
dinh <dinh.test263@hotmail.com>
2020-02-20 15:08:53 +0700

    edit file

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,12 @@ import java.util.stream.IntStream;
 public class Hello {
     public static void main(String[] args){
         System.out.println("=========start testing===========");
-        IntStream.range(1,100).forEach(x->System.out.println(x));
+        System.out.println("**********only get the odd number************");
+        IntStream.range(1,100).forEach(x->{
+            if(x%2==0){
+                System.out.println(x);
+            }
+        });
         System.out.println("=========done testing===========");
     }
 }
